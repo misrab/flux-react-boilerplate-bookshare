a6f2ba9fa821e0f31067103a5a4f85c520e9f803
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2015-12-11 17:29:05 +0800

    automatic commit

--- js/helpers/config.js
+++ js/helpers/config.js
@@ -3,7 +3,10 @@ define([
 ], function() {
 	var result = {};
 
-	result.API_URL = 'http://localhost:8000/api/v1';
+
+	var root = window.location.href.indexOf("localhost") !== -1 ? 'localhost' : 'http://ec2-52-35-87-150.us-west-2.compute.amazonaws.com';
+
+	result.API_URL = 'http://'+root+':8000/api/v1';
 
 	// for the 'see more' button on the feed
 	result.POST_OFFSET_INCREMENT = 10;  // mirrored on server
